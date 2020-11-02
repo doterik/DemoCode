39310a1795bd97f2ed01def53872d86af7f90bed
Erik <qdtgema@hotmail.com>
2020-11-02 15:08:08 +0100

    DemoWithMultipleMain

---
+++ DemoUtil/JonSkeet.DemoUtil.Test/DemoWithMultipleMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace JonSkeet.DemoUtil.Test
+{
+	[Description("More than one Main Method.")]
+	class DemoWithMultipleMain
+	{
+		static void Main(string args) => Console.WriteLine("Main(string args)");
+		static void Main() => Console.WriteLine("Main()");
+		static void Main(int i) => Console.WriteLine("Main(int i)");
+		static void Main(string[] args) => Console.WriteLine("Main(string [] args)");
+	}
+}
